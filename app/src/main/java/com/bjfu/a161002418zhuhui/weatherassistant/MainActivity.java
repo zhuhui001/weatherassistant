31f7c03527506a194887ee1f1b6c6aef7579913e
zhuhui <15901202236@163.com>
2020-02-05 16:18:40 +0800

    First commit

---
+++ app/src/main/java/com/bjfu/a161002418zhuhui/weatherassistant/MainActivity.java
@@ -0,0 +1,13 @@
+package com.bjfu.a161002418zhuhui.weatherassistant;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
